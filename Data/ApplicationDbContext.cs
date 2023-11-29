621ea1ff77d252fd969616b3a9bc29adf93ed04a
Thanh Nguyen <71760329+Thanhibanani@users.noreply.github.com>
2023-11-29 16:22:54 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RentHive._2._2.Models;
+
+namespace RentHive._2._2.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RentHive._2._2.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
